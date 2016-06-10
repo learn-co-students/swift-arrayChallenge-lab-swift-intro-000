045995026b7e703ebd86e82e663aebb62359c31f
JimCampagno <jimcampagno@gmail.com>
2016-06-10 13:39:01 -0400

    add xcode project

---
+++ ArrayChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArrayChallenge
+//
+//  Created by James Campagno on 6/10/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
