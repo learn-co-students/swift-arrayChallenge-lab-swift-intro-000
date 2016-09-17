8f4f569066627760d3943253424d422dd7340597
JimCampagno <jimcampagno@gmail.com>
2016-09-17 09:33:20 -0400

    Remove old code from ViewController

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -10,32 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // Our blank piece of paper
-    var shoppingList: [String] = []
-    
-    // Quantity of items
-    var quantityOfItems: [Int] = [6, 4, 12, 4]
-    
-    // Name of items
-    var nameOfItems: [String] = ["Bananas", "Apples", "Eggs", "Rolls"]
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // After you're done implementing your method, call on it here. You can remove this example below
-        
-        thisIsAnExample()
-        
     }
-    
-    // This is just an example - you can remove this.
-    func thisIsAnExample() {
-        print("This ist just an example, you can remvove this.")
-    }
-    
-    
-    // Implement your method here
-
     
 }
