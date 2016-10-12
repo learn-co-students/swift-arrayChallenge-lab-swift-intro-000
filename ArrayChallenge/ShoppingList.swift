ce0e55f5707cf2d0220eeb193b2c670dd17cc5d3
jakim54 <james.kim@huffingtonpost.com>
2016-10-12 14:13:43 -0400

    testing out ideas

--- ArrayChallenge/ShoppingList.swift
+++ ArrayChallenge/ShoppingList.swift
@@ -9,27 +9,60 @@
 
 class ShoppingList {
     
+    let itemsName = ["Bananas", "Apples", "Eggs", "Rolls"]
+    let amountOfItems = ["6", "4", "12", "4"]
     
     func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String] {
+        var theNewArray: [String] = []
         
-       
-        // Implement this function
-        
-        //print(items)
-        //print(amounts)
-        //return nameOfEnumeratedArray
-        
-    }
-    
-}
+        if theNewArray.isEmpty {
+            for itemName in items.enumerated() {
+                theNewArray.append(itemName)
+                for quantity in amounts {
+                    theNewArray.append(quantity)
+                }
+            }
+        }
+        print(theNewArray)
+        return theNewArray
+    } // end of function
+
+} // end of class
 
 /*
 To call this function
 createShoppingList(withItems: AllTheItems, amountOfEachItem: amountOfItems)
  
+ //        print("\(items)")
+ //        print("(\(amounts))")
+ 
 The array returned from this function needs to look like this:
 ["1. Bananas(6)", "2. Apples(4)", "3. Eggs(12)", "4. Rolls(4)"]
  TODO:
- - Use enumerated()
- - Two arrays, each specific to its list, must be added together by a loop, and then enumerated and printed. Concatenate with a for loop.
+ - Have to use enumerated()
+ - Two arrays, each specific to its list, enumerated via for loop and printed.
+ - Concatenate with a for loop.
+ 
+ WIP:
+ let itemsName = ["Bananas", "Apples", "Eggs", "Rolls"]
+ let amountOfItems = ["6", "4", "12", "4"]
+ 
+ func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String] {
+ var theNewArray: [String] = []
+ 
+ if theNewArray.isEmpty {
+ for itemName in items {
+ theNewArray.append(itemName)
+ }
+ for quantity in amounts {
+ theNewArray.append(quantity)
+ }
+ }
+ print(theNewArray)
+ return theNewArray
+ }
+ 
+ // this prints ["Bananas", "Apples", "Eggs", "Rolls", "6", "4", "12", "4"]
+ // enumerated() would fix this, but now need to sort them via index, or concat somehow.
+ 
 */
