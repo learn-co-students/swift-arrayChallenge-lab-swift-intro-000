71ba93666e064734956924cc58e4e45feaf17ec7
jakim54 <james.kim@huffingtonpost.com>
2016-10-12 16:38:45 -0400

    solved

--- ArrayChallenge/ShoppingList.swift
+++ ArrayChallenge/ShoppingList.swift
@@ -12,69 +12,13 @@ class ShoppingList {
     let itemsName = ["Bananas", "Apples", "Eggs", "Rolls"]
     let amountOfItems = ["6", "4", "12", "4"]
     
-    func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String] {
-        var theNewArray: [String] = []
-        
-        if theNewArray.isEmpty {
-            for itemName in items.enumerated() {
-                theNewArray.append(itemName)
-                for quantity in amounts {
-                    theNewArray.append(quantity)
-                }
-            }
+    func createShoppingList(withItems: [String],  amountOfEachItem: [String]) ->  [String] {
+        var newArray: [String] = []
+        for (index, value) in amountOfEachItem.enumerated() {
+            newArray.append("\(index + 1). \(withItems[index])(\(value))")
         }
-        print(theNewArray)
-        return theNewArray
-    } // end of function
-
-} // end of class
-
-// On the first for loop, use enumerated, keep a counter and then for the second for loop use that counter to match the index and append it if it matches.
-
-/*
-To call this function
-createShoppingList(withItems: AllTheItems, amountOfEachItem: amountOfItems)
- 
- //        print("\(items)")
- //        print("(\(amounts))")
- 
-The array returned from this function needs to look like this:
-["1. Bananas(6)", "2. Apples(4)", "3. Eggs(12)", "4. Rolls(4)"]
- TODO:
- - Have to use enumerated()
- - Two arrays, each specific to its list, enumerated via for loop and printed.
- - Concatenate with a for loop.
- 
- WIP:
- let itemsName = ["Bananas", "Apples", "Eggs", "Rolls"]
- let amountOfItems = ["6", "4", "12", "4"]
- 
- func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String] {
- var theNewArray: [String] = []
- 
- if theNewArray.isEmpty {
- for itemName in items {
- theNewArray.append(itemName)
- }
- for quantity in amounts {
- theNewArray.append(quantity)
- }
- }
- print(theNewArray)
- return theNewArray
- }
- 
- // this prints ["Bananas", "Apples", "Eggs", "Rolls", "6", "4", "12", "4"]
- // enumerated() would fix this, but now need to sort them via index, or concat somehow.
- 
- ==============================================
- 
- var iceCreamFlavors = ["Vanilla", "Chocolate", "Strawberry"]
- var iceCreamLeft = ["20", "5", "50"]
- 
- for index in 0..<iceCreamFlavors.count {
- print("\(iceCreamLeft[index])::\(iceCreamFlavors[index])")
- }
- // this works without using enumerated()
- 
-*/
+        
+        print(newArray)
+        return newArray
+    }
+}
