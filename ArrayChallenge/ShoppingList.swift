b0be837c1cd5808b873a10fd24ff5dca01c23c34
jajuki13 <khkim1776@gmail.com>
2016-10-11 23:30:18 -0400

    easy lab, wrote todo, will do tomorrow

--- ArrayChallenge/ShoppingList.swift
+++ ArrayChallenge/ShoppingList.swift
@@ -15,8 +15,21 @@ class ShoppingList {
        
         // Implement this function
         
-        
+        //print(items)
+        //print(amounts)
+        //return nameOfEnumeratedArray
         
     }
     
 }
+
+/*
+To call this function
+createShoppingList(withItems: AllTheItems, amountOfEachItem: amountOfItems)
+ 
+The array returned from this function needs to look like this:
+["1. Bananas(6)", "2. Apples(4)", "3. Eggs(12)", "4. Rolls(4)"]
+ TODO:
+ - Use enumerated()
+ - Two arrays, each specific to its list, must be added together by a loop, and then enumerated and printed. Concatenate with a for loop.
+*/
