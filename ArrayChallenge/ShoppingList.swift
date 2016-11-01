03c0673c5cb1ef8a5b258eb677f8446ead1fd84c
Blake Minho Kim <blakeminhokim@Blakes-MacBook-Pro.local>
2016-11-01 00:32:47 -0400

    finished

--- ArrayChallenge/ShoppingList.swift
+++ ArrayChallenge/ShoppingList.swift
@@ -12,11 +12,16 @@ class ShoppingList {
     
     func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String] {
         
-       
-        // Implement this function
+        var completeList : [String] = []
         
+        for (index, item) in items.enumerated(){
+            completeList.append("\(index) + 1). \(item)(\(amounts[index]))")
+        }
         
-        
+        return(completeList)
     }
     
 }
+
+
+
