3cd44af6410d41da0204a538c2f70afb4a0fefe7
Autumn Raine <autumnraine@Ambers-MBP.home>
2016-12-25 20:13:13 -0500

    Lab Complete

--- ArrayChallenge/ShoppingList.swift
+++ ArrayChallenge/ShoppingList.swift
@@ -11,12 +11,14 @@ class ShoppingList {
     
     
     func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String] {
+        var shoppingList: [String] = []
         
-       
-        // Implement this function
-        
-        
-        
+        for (index, item) in items.enumerated() {
+            let amount = amounts[index]
+            let shoppingItem = "\(index+1). \(item)(\(amount)"
+            shoppingList.append(shoppingItem)
+        }
+        return shoppingList
     }
     
 }
