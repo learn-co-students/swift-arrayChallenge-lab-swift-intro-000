b4c2f798150253d9d52c7256e499e52c217cd442
felixvdl <vandelanottefelix@gmail.com>
2017-05-19 16:13:41 -0400

    complete

--- ArrayChallenge/ShoppingList.swift
+++ ArrayChallenge/ShoppingList.swift
@@ -12,11 +12,17 @@ class ShoppingList {
     
     func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String] {
         
-       
-        // Implement this function
+        var result: [String] = []
+        for (index, item) in items.enumerated() {
+            result.append("\(index + 1). \(item)(\(amounts[index]))")
+        }
+        return result
+        
         
         
         
     }
     
 }
+
+
