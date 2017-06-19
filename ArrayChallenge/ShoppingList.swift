765336358f094198ceb60dcc68a5b324d2b87662
terminalzzz <zliu.msb@gmail.com>
2017-06-18 20:57:07 -0400

    my commit

--- ArrayChallenge/ShoppingList.swift
+++ ArrayChallenge/ShoppingList.swift
@@ -11,12 +11,14 @@ class ShoppingList {
     
     
     func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String] {
-        
-       
-        // Implement this function
-        
-        
-        
-    }
+        var output: [String] = []
     
+        for (index, item) in items.enumerated() {
+            output.append("\(index+1). \(item)(\(amounts[index]))")
+        }
+        print(output)
+        return output
+    }
 }
+
+
