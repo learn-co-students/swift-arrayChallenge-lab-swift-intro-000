9f32fe0403b25cab8e2cb5010e6489fdbb396c10
gbwest <gordon_west@msn.com>
2017-07-18 23:22:02 -0700

    Completed array challenge

--- ArrayChallenge/ShoppingList.swift
+++ ArrayChallenge/ShoppingList.swift
@@ -12,10 +12,14 @@ class ShoppingList {
     
     func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String] {
         
-       
-        // Implement this function
+        var repairedList: [String] = []
         
+        for (index, item) in items.enumerated() {
+           print("\(index), \(item)")
+           repairedList.append("\(index + 1). \(item)(\(amounts[index]))")
+        }
         
+        return repairedList
         
     }
     
