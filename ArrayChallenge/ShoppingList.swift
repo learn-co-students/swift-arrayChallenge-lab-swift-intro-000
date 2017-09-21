c13610d64a276eb69c3b436e01b913621a5c792b
stephanos <stephanos@goldmail.etsu.edu>
2017-09-21 15:15:25 -0700

    Completed

--- ArrayChallenge/ShoppingList.swift
+++ ArrayChallenge/ShoppingList.swift
@@ -12,11 +12,14 @@ class ShoppingList {
     
     func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String] {
         
-       
+        var shoppingList : [String] = []
         // Implement this function
-        
-        
-        
+        for i in 0...(items.count - 1) {
+            shoppingList.append(items[i] + "(\(amounts[i]))")
+        }
+        for (index, item) in shoppingList.enumerated() {
+            shoppingList[index] = "\(index + 1). \(item)"
+        }
+        return shoppingList
     }
-    
 }
