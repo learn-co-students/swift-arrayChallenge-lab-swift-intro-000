a3c32aa90b02db5e27dd118835e835e2f25af49a
Gustavo Picciuto <gustavo.picciutomaury@live.com>
2018-09-23 23:11:34 -0400

    Complete problems

--- ArrayChallenge/ShoppingList.swift
+++ ArrayChallenge/ShoppingList.swift
@@ -5,18 +5,20 @@
 //  Created by Jim Campagno on 9/17/16.
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
-
+import UIKit
+import Foundation
 
 class ShoppingList {
     
-    
     func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String] {
-        
-       
-        // Implement this function
-        
-        
-        
+        var completedList = [String]()
+        var index = 0
+        for (item, amount) in zip(items, amounts){
+            index += 1
+            completedList.append("\(index). \(item)(\(amount))")
+        }
+        return completedList
     }
-    
 }
+
+
